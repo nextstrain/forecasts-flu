b1844c93236acd1b6e2b502ebc9dafcccc5eaff9
james hadfield <jameshadfield@users.noreply.github.com>
2026-05-04 12:29:51 +1200

    [viz] version 0.4.0
    Includes a large refactor of `main.tsx` to make it much easier to
    read. Also enables forthcoming relative-GA plots, but I'll remove
    them in a subsequent commit as they're not ready for production.

---
+++ viz/src/dataConfig.ts
@@ -0,0 +1,60 @@
+import type { DatasetConfig } from '@nextstrain/evofr-viz';
+
+
+const sites: DatasetConfig['sites'] = {
+  freq: {
+    estimateSites: ['freq', 'freq_forecast'],
+    ps_point_estimator: 'median',
+    ps_interval_estimator: ['HDI_95_lower', 'HDI_95_upper'],
+    interval_name: "95% HDI",
+    raw_site: 'raw_freq',
+    raw_name: 'Raw Data',
+    smoothed_site: 'smoothed_raw_freq',
+    smoothed_name: 'Smoothed Raw Data',
+  },
+  // Following enables relative GA calculations, but I haven't validated
+  // these yet so don't use in production
+  relativeGA: {enable: true},
+};
+
+export function createConfig(tabSelected: string, modelDate: string, variantClassification: string): DatasetConfig {
+  const config: DatasetConfig = {
+    modelName: tabSelected,
+    modelUrl: _modelUrl(variantClassification, tabSelected, modelDate),
+    sites: { ...sites },
+  }
+  
+  /* TODO XXX - following should be in the JSON, but is here to show the capability */
+  if (tabSelected === "h1n1pdm/country") {
+    config.locationHierarchy = new Map([
+      [
+        'Region',
+        new Map([
+          ['Oceania', ['Australia']],
+          ['North America', ['Canada', 'USA']],
+          ['Europe', ['Denmark', 'France', 'Germany', 'Italy', 'Netherlands', 'Norway', 'Portugal', 'Spain', 'United Kingdom']],
+        ])
+      ]
+    ]);
+  }
+  
+  return config;
+}
+
+
+function _modelUrl(variantClassification, subtypeResolution, modelDate) {
+  let url = `https://data.nextstrain.org/files/workflows/forecasts-flu/gisaid/${variantClassification}/${subtypeResolution}/mlr/MLR_results.json`;
+
+  if (modelDate) {
+    // Fall back to the original URL format for model results generated prior to
+    // our support for multiple data provenances and variant classifications.
+    if (Date.parse(modelDate) < Date.parse("2025-12-23")) {
+      url = `https://data.nextstrain.org/files/workflows/forecasts-flu/${subtypeResolution}/mlr/MLR_results.json`;
+    }
+
+    url = url.replace(/([^/]+)$/, `${modelDate}_MLR_results.json`);
+  }
+
+  return url;
+}
+
